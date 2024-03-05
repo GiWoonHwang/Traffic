2da6f9c555bc015cc2ffd867cc0509e9b7442dfe
GiWoonHwang <rlaejrqo465@naver.com>
2024-03-05 16:11:04 +0900

    init project

---
+++ Dustin/src/main/java/com/example/fastcampusmysql/DustinApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastcampusmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DustinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DustinApplication.class, args);
+    }
+
+}
