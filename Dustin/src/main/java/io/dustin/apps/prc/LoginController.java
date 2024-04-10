369ce030324585dae1d71c19ee213fb0e87b6d22
GiWoonHwang <rlaejrqo465@naver.com>
2024-04-10 11:47:14 +0900

    redis로 세션 구현해보기

---
+++ Dustin/src/main/java/io/dustin/apps/prc/LoginController.java
@@ -0,0 +1,27 @@
+package io.dustin.apps.prc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class LoginController {
+
+
+    @GetMapping("/login")
+    public  String Login(HttpSession session, @RequestParam String name) {
+        // 세션에 파라미터로 들어온 name을 저장한다
+        session.setAttribute("name",name);
+        return "saved";
+
+    }
+
+    @GetMapping("/myName")
+    public  String Login(HttpSession session) {
+        String myName = (String)session.getAttribute("name");
+
+        return myName;
+    }
+}
--- Dustin/src/main/java/io/dustin/apps/prc/RedisBasicPrcController.java
+++ Dustin/src/main/java/io/dustin/apps/prc/RedisBasicPrcController.java
@@ -1,4 +1,4 @@
-package io.dustin.apps;
+package io.dustin.apps.prc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
